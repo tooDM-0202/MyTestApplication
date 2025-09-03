bd0011b2539f4e073bfbbdf611a303eb806a3331
Arisa Yoshio <184593927+tooDM-0202@users.noreply.github.com>
2025-09-03 14:31:19 +0900

    make going　screen
    active2に画面遷移を実装

--- app/src/main/java/jp/ac/meijou/android/s241205183/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205183/MainActivity2.java
@@ -1,6 +1,9 @@
 package jp.ac.meijou.android.s241205183;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205183.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s241205183.databinding.ActivityMainBinding;
+
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+
+        ActivityMain2Binding binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //明示的
+        binding.buttonNumA.setOnClickListener((View view) -> {
+            var intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+                });
+
+        //暗示的
+        binding.buttonNumB.setOnClickListener(view -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
+        });
+
+
+
     }
 }
