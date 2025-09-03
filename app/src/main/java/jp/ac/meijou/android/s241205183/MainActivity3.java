b1ded347bf7ad4db477e0dc5b4bc7048e63705f7
Arisa Yoshio <184593927+tooDM-0202@users.noreply.github.com>
2025-09-03 13:32:29 +0900

    add little function
    電卓の機能を試し書き

--- app/src/main/java/jp/ac/meijou/android/s241205183/MainActivity3.java
+++ app/src/main/java/jp/ac/meijou/android/s241205183/MainActivity3.java
@@ -8,8 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205183.databinding.ActivityMain3Binding;
+import jp.ac.meijou.android.s241205183.databinding.ActivityMainBinding;
+
 public class MainActivity3 extends AppCompatActivity {
 
+    private ActivityMain3Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,22 @@ public class MainActivity3 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+/*
+機能作りかけ
+        binding.buttonNumberAC.setOnClickListener(view -> {
+            binding.result.setText(0);
+        });
+
+        binding.buttonNumber1.setOnClickListener(view -> {
+            float res = Float.parseFloat(binding.result.getText().toString());
+            binding.result.setText((int) (res+1));
+        });
+
+     */
+
+
+
+
+
     }
 }
