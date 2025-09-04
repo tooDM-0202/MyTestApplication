42e285c248404ff0d9fd75cbd3aee55005b9bfa3
Arisa Yoshio <184593927+tooDM-0202@users.noreply.github.com>
2025-09-04 14:08:51 +0900

    make activity6  appear image
    ネットから画像を取得して表示するものを作った

---
+++ app/src/main/java/jp/ac/meijou/android/s241205183/MainActivity6.java
@@ -0,0 +1,92 @@
+package jp.ac.meijou.android.s241205183;
+
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import jp.ac.meijou.android.s241205183.databinding.ActivityMain5Binding;
+import jp.ac.meijou.android.s241205183.databinding.ActivityMain6Binding;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    private ActivityMain6Binding binding;
+    private final OkHttpClient okHttpClient = new OkHttpClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityMain6Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.Getbutton.setOnClickListener(view ->{
+
+            //binding.imageView.setColorFilter(Color.WHITE);
+
+            var text = binding.editTextText2.getText().toString();
+            // textパラメータをつけたURLの作成
+            var url = Uri.parse("https://placehold.jp/350x350.png")
+                    .buildUpon()
+                    .appendQueryParameter("text", text)
+                    .build()
+                    .toString();
+
+            // 画像の取得開始
+            getImage(url);
+
+
+
+        });
+
+      //  getImage("https://placehold.jp/350x350.png");
+    }
+
+    private  void  getImage(String url){
+        var request = new Request.Builder()
+                .url(url)
+                .build();
+
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+            }
+
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+
+                runOnUiThread(() -> binding.imageView.setImageBitmap(bitmap));
+
+            }
+        });
+
+
+
+    }
+}
